f933a37c41a6a0359dd24ae62fdeaf56fb03d73e
broklyn777 <50321354+broklyn777@users.noreply.github.com>
2022-08-16 11:23:32 +0200

    favicon added

--- pages/_app.js
+++ pages/_app.js
@@ -5,6 +5,7 @@ import '../styles/globals.css'
 function MyApp({ Component, pageProps }) {
   return <>
     <Head>
+      <link rel="shortcut icon" href="/static/ostana-favicon.ico" />
       <meta content="width=device-width, initial-scale=1" name="viewport" />
     </Head>
 

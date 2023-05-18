15401501a149a7e20f54d80c442c9f98d9ca996e
broklyn777 <50321354+broklyn777@users.noreply.github.com>
2023-05-18 17:11:02 +0200

    add analytics

--- components/BjProfil.jsx
+++ components/BjProfil.jsx
@@ -1,10 +1,13 @@
 /* eslint-disable @next/next/no-img-element */
+import Image from 'next/image';
 
 
 export default function BjProfil() {
   return (
     <div className="w-4/5 min-w-[350px] pb-1 m-auto  -mt-8 ">
           <img src="https://res.cloudinary.com/einar/image/upload/c_scale,w_1460/v1660602354/%C3%B6stan%C3%A5%20service/bjprofil-002_av3xes.webp" alt="östanå hemservice, trädgårdsservice, trädgårdshjälp"
+              
+                  layout="responsive"
             className=" border-2 border-orange-600  rounded-2xl shadow-2xl -rotate-3" />
         </div>  )
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,21 +1,42 @@
+
+/* eslint-disable @next/next/inline-script-id */
 import Head from 'next/head'
 import Layout from '../components/Layout'
 import '../styles/globals.css'
+import Script from 'next/script'
 
 
 function MyApp({ Component, pageProps }) {
-  return <>
-    
+ return <>
     <Head>
-      <link rel="shortcut icon" href="/static/ostana-favicon.ico" />
-      <meta content="width=device-width, initial-scale=1" name="viewport" />
-    </Head>
-   
+     <link rel="shortcut icon" href="/static/ostana-favicon.ico" />
+     <meta content="width=device-width, initial-scale=1" name="viewport" />
+   </Head>
+ 
+     <Script strategy="afterInteractive"  src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`} />
+     <Script>
+ {`  window.dataLayer = window.dataLayer || [];
+ function gtag(){dataLayer.push(arguments);}
+ gtag('js', new Date());
 
 
-    <Layout>
-      <Component {...pageProps} />
-    </Layout></>
+ gtag('config', 'G-082LQ4GE44'); `}
+</Script>
+
+
+   <Layout>
+     <Component {...pageProps} />
+    
+   </Layout>
+
+
+   </>
 }
 
+
 export default MyApp
+
+
+
+
+
